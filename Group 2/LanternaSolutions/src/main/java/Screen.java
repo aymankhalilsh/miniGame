9b6ec99a77d51709f8f8ee17579cbff60e86f585
Ayman Shaheen <aymankhalilsh@gmail.com>
2022-08-18 17:02:16 +0200

    lATEST UPDATE

--- Group 2/LanternaSolutions/src/main/java/Screen.java
+++ Group 2/LanternaSolutions/src/main/java/Screen.java
@@ -1,18 +1,31 @@
+import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
 import java.util.Random;
+
 public class Screen {
-    char [][] array =new char[80][24];
+
+    public static final TextColor
+            WHITE = new TextColor.RGB(192, 192, 192),
+            BLACK = new TextColor.RGB(0, 0, 0),
+            BLUE = new TextColor.RGB(0, 0, 255),
+            YELLOW = new TextColor.RGB(255, 255, 0),
+            GREEN = new TextColor.RGB(0, 255, 0),
+            RED = new TextColor.RGB(255, 0, 0);
+    char[][] array = new char[80][24];
     DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
     Terminal terminal;
-    public void printToScreen(int col, int row, char c) throws IOException {
+
+    public void printToScreen(int col, int row, char c, TextColor foreGround, TextColor backGround) throws IOException {
 
         terminal.setCursorPosition(col, row);
+        terminal.setForegroundColor(foreGround);
+        terminal.setBackgroundColor(backGround);
         terminal.putCharacter(c);
-        array[col-1][row-1] =c;
+        array[col - 1][row - 1] = c;
         terminal.flush();
     }
 
@@ -20,9 +33,9 @@ public class Screen {
         return terminal.pollInput();
     }
 
-    public char getChar(int col, int row){
+    public char getChar(int col, int row) {
 
-        return array[col-1][row-1];
+        return array[col - 1][row - 1];
 
     }
 
@@ -31,7 +44,21 @@ public class Screen {
         terminal.setCursorVisible(false);
 
     }
+
     public void close() throws IOException {
-       terminal.close();
+        terminal.close();
+    }
+
+    public void printGameWon(int col, int row, String s, TextColor forGround, TextColor backGround) throws IOException {
+        for (int j = 0; j < s.length(); j++) {
+            printToScreen(col + j, row, s.charAt(j), forGround, backGround);
+
+        }
+    }
+
+    public void printGameOver(int col, int row, String s, TextColor forGround, TextColor backGround) throws IOException {
+        for (int j = 0; j < s.length(); j++) {
+            printToScreen(col + j, row, s.charAt(j), forGround, backGround);
+        }
     }
 }
