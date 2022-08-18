c3058b49d3eeff57f617e06099b46e513f78091b
Ayman Shaheen <aymankhalilsh@gmail.com>
2022-08-18 14:05:42 +0200

    lATEST UPDATE

--- Group 2/LanternaSolutions/src/main/java/Main.java
+++ Group 2/LanternaSolutions/src/main/java/Main.java
@@ -1,41 +1,27 @@
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
-
 import java.io.IOException;
 import java.util.Random;
-
 public class Main {
     static Screen screen;
     final static char block = '\u2588';
+    final static char player = 'X';
+    static int x = 1; //col
+    static int y = 22;//row
+    static int a = 78; //col
+    static int b = 21;//row
+    final static char arrow = '\u21db'; //
+    final static char bomb = '\u1F4A'; //
 
     public static void main(String[] args) throws Exception {
-
-        int x = 1; //col
-        int y = 22;//row
-        int a = 78; //col
-        int b = 21;//row
-        final char arrow = '\u21db'; //
-        final char bomb = '\u1F4A'; //
-        //final char bomb2 = '\u2F2E'; // monster
-        final char block2 = '\u2588';
-        final char player = 'X';
 
         screen = new Screen();
         screen.printToScreen(x, y, player);
         screen.printToScreen(a, b, arrow);
 
-        pirntBoundries();
-
-        Position bombPosition = new Position(x,y);
-        for(int i =0; i<15;i++) {
-            Random r = new Random();
-            bombPosition = new Position(r.nextInt(76) +2, r.nextInt(20)+2);
-            if(screen.getChar(bombPosition.x,bombPosition.y)!=block) {
-                screen.printToScreen(bombPosition.x, bombPosition.y, bomb);
-            }
-        }
-
-        // increase size of the Arrow unicode
+        printBoundries();
+        mazeObstacles();
+        bombNumber();
 
         boolean continueReadingInput = true;
         while (continueReadingInput) {
@@ -50,15 +36,13 @@ public class Main {
             Character c = keyStroke.getCharacter(); // used Character instead of char because it might be null
 
             System.out.println("keyStroke.getKeyType(): " + type + " keyStroke.getCharacter(): " + c);
-
             if (c == Character.valueOf('q')) {
                 continueReadingInput = false;
                 screen.close();
                 System.out.println("quit");
             }
-
-            int oldX = x; // save old position x
-            int oldY = y; // save old position y
+            int oldX = x;
+            int oldY = y;
             switch (keyStroke.getKeyType()) {
                 case ArrowDown:
                     y += 1;
@@ -73,59 +57,51 @@ public class Main {
                     x -= 1;
                     break;
             }
-
-//             ***** more than one if else statement solved
-
-            if (screen.getChar(x, y) == block || screen.getChar(x, y) == block || screen.getChar(x, y) == block) {
+            if (screen.getChar(x, y) == block) {
                 x = oldX;
                 y = oldY;
+            } else if (screen.getChar(x, y) == bomb) {
+                screen.printToScreen(35, 12, 'B');
+                screen.printToScreen(36, 12, 'A');
+                screen.printToScreen(37, 12, 'N');
+                screen.printToScreen(38, 12, 'G');
+                screen.printToScreen(39, 12, ',');
+                screen.printToScreen(40, 12, 'G');
+                screen.printToScreen(41, 12, 'A');
+                screen.printToScreen(42, 12, 'M');
+                screen.printToScreen(43, 12, 'E');
+                screen.printToScreen(44, 12, ' ');
+                screen.printToScreen(45, 12, 'O');
+                screen.printToScreen(46, 12, 'V');
+                screen.printToScreen(47, 12, 'E');
+                screen.printToScreen(48, 12, 'R');
+                screen.printToScreen(49, 12, '!');
+                Thread.sleep(5000);
+                screen.close();
+                continueReadingInput = false;
             } else {
-//
                 screen.printToScreen(oldX, oldY, ' ');
                 screen.printToScreen(x, y, player);
             }
-
-
             Position p = new Position(a, b);
-            for (int i = 0; i < 15; i++) {
-                if ((bombPosition.x == x && bombPosition.y == y) || (p.x == x && p.y == y)) { // add 2nd bomb statement
-                    screen.close();
-                    continueReadingInput = false;
-                }
+            if ((p.x == x && p.y == y)) {
+                screen.printToScreen(35, 12, 'G');
+                screen.printToScreen(36, 12, 'A');
+                screen.printToScreen(37, 12, 'M');
+                screen.printToScreen(38, 12, 'E');
+                screen.printToScreen(39, 12, ' ');
+                screen.printToScreen(40, 12, 'W');
+                screen.printToScreen(41, 12, 'O');
+                screen.printToScreen(42, 12, 'N');
+                screen.printToScreen(43, 12, '!');
+                Thread.sleep(5000);
+                screen.close();
+                continueReadingInput = false;
             }
         }
     }
+    public static void printBoundries() throws IOException {
 
-    public static void pirntBoundries() throws IOException {
-
-        for (int i = 0; i < 10; i++) {
-            screen.printToScreen(i + 1, 20, block);
-            screen.printToScreen(i + 6, 6, block);
-
-        }
-
-        for (int i = 0; i < 10; i++) {
-            screen.printToScreen(15, i +6, block);//right corner curve
-            //screen.printToScreen(i + 6, 6, block);
-
-        }
-
-        for (int i = 0; i < 6; i++) {
-            screen.printToScreen(i + 30, 16, block);// L shape
-            screen.printToScreen(i + 15, 16, block);// Z Shape
-            screen.printToScreen(i + 24, 10, block);// long T shap
-
-        }
-        for (int i = 0; i < 16; i++){ // 16 to 5 and 5 to16
-            screen.printToScreen(30, i + 1, block); // vertical from horizontal boundar
-        }
-        for (int i = 0; i < 5; i++){ // 16 to 5 and 5 to16
-            screen.printToScreen(36, i + 16, block);
-            //screen.printToScreen(21, i + 19, block);
-        }
-        for (int i = 0; i < 8; i++){ // continuation of z shape
-            screen.printToScreen(21, i + 16, block);
-        }
         for (int i = 0; i < 78; i++) {                          // printing horizontal boundary
             screen.printToScreen(i + 1, 23, block);
             screen.printToScreen(i + 1, 1, block);
@@ -134,17 +110,41 @@ public class Main {
             screen.printToScreen(1, i + 1, block);
             screen.printToScreen(79, i + 1, block);
         }
+    }
+
+    public static void mazeObstacles() throws IOException {
+        for (int i = 0; i < 10; i++) {
+            screen.printToScreen(i + 1, 20, block);
+            screen.printToScreen(i + 6, 6, block);
+        }
+        for (int i = 0; i < 10; i++) {
+            screen.printToScreen(15, i + 6, block);//right corner curve
+        }
+
+        for (int i = 0; i < 6; i++) {
+            screen.printToScreen(i + 30, 16, block);// L shape
+            screen.printToScreen(i + 15, 16, block);// Z Shape
+            screen.printToScreen(i + 24, 10, block);// long T shap
+        }
+        for (int i = 0; i < 16; i++) { // 16 to 5 and 5 to16
+            screen.printToScreen(30, i + 1, block); // vertical from horizontal boundar
+        }
+        for (int i = 0; i < 5; i++) { // 16 to 5 and 5 to16
+            screen.printToScreen(36, i + 16, block);
+            //screen.printToScreen(21, i + 19, block);
+        }
+        for (int i = 0; i < 8; i++) { // continuation of z shape
+            screen.printToScreen(21, i + 16, block);
+        }
 
         for (int i = 0; i < 8; i++) {                          // print 2 vertical boundary on right
             screen.printToScreen(70, i + 1, block);
             screen.printToScreen(70, i + 16, block);
         }
-
         for (int i = 0; i < 20; i++) {                          // printing horizontal across vertical boundary
             screen.printToScreen(i + 59, 11, block);
             screen.printToScreen(i + 39, 5, block);//long horizontal
         }
-
         for (int i = 0; i < 15; i++) {                          // print 2 vertical crossing horizontal
             screen.printToScreen(59, i + 5, block);
             screen.printToScreen(45, i + 9, block);
@@ -152,6 +152,16 @@ public class Main {
 
         for (int i = 0; i < 10; i++) {                          // print 2 vertical crossing horizontal
             screen.printToScreen(39, i + 5, block);
+        }
+    }
+    public static void bombNumber() throws IOException {
+        Position bombPosition;
+        for (int i = 0; i < 100; i++) {
+            Random r = new Random();
+            bombPosition = new Position(r.nextInt(76) + 2, r.nextInt(20) + 2);
+            if (screen.getChar(bombPosition.x, bombPosition.y) != block) {
+                screen.printToScreen(bombPosition.x, bombPosition.y, bomb);
+            }
         }
     }
 }
--- Group 2/LanternaSolutions/src/main/java/Screen.java
+++ Group 2/LanternaSolutions/src/main/java/Screen.java
@@ -4,11 +4,8 @@ import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
 import java.util.Random;
-
 public class Screen {
-
     char [][] array =new char[80][24];
-
     DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
     Terminal terminal;
     public void printToScreen(int col, int row, char c) throws IOException {
@@ -17,7 +14,6 @@ public class Screen {
         terminal.putCharacter(c);
         array[col-1][row-1] =c;
         terminal.flush();
-
     }
 
     public KeyStroke getInput() throws IOException {
@@ -35,18 +31,7 @@ public class Screen {
         terminal.setCursorVisible(false);
 
     }
-
     public void close() throws IOException {
        terminal.close();
     }
-
-/*    public void bomb(){
-        Random r = new Random();
-        Position bombPosition = new Position(r.nextInt(80), r.nextInt(24));
-      *//*  terminal.setCursorPosition(bombPosition.x, bombPosition.y);
-        terminal.putCharacter(bomb);*//*
-       printToScreen(bombPosition.x, bombPosition.y, );
-
-        terminal.flush();
-    }*/
 }
